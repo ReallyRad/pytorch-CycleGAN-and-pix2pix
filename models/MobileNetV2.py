206f914534e66124ae91610985451f0d5aab65f0
ReallyRad <arthur.pointeau@gmail.com>
2020-08-07 19:02:02 +0100

    use pytorch's url

--- models/MobileNetV2.py
+++ models/MobileNetV2.py
@@ -183,6 +183,6 @@ def mobilenet_v2(pretrained=False, progress=True, **kwargs):
         except ImportError:
             from torch.utils.model_zoo import load_url as load_state_dict_from_url
         state_dict = load_state_dict_from_url(
-            'https://www.dropbox.com/s/47tyzpofuuyyv1b/mobilenetv2_1.0-f2a8633.pth.tar?dl=1', progress=True)
+            'https://download.pytorch.org/models/mobilenet_v2-b0353104.pth', progress=True)
         model.load_state_dict(state_dict)
     return model
