a99d25c10f4e8040d55cab8d8aac440dc0e861ca
ReallyRad <arthur.pointeau@gmail.com>
2020-08-07 21:34:27 +0100

    make export script

---
+++ export.py
@@ -0,0 +1,33 @@
+import os
+from options.test_options import TestOptions
+from data import create_dataset
+from models import create_model
+from util.visualizer import save_images
+from util import html
+import torch
+
+if __name__ == '__main__':
+    opt = TestOptions().parse()  # get test options
+    # hard-code some parameters for test
+    opt.num_threads = 0   # test code only supports num_threads =
+    opt.batch_size = 1    # test code only supports batch_size = 1
+    opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
+    opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
+    opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
+    dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
+    model = create_model(opt)      # create a model given opt.model and other options
+    model.setup(opt)               # regular setup: load and print networks; create schedulers
+
+    # original saved file with DataParallel
+    state_dict = torch.load('/content/drive/My Drive/Training Data/checkpoints/human2cat_pix2pix/latest_net_G.pth')
+    # create new OrderedDict that does not contain module.
+    from collections import OrderedDict
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        name = k[7:] # remove module.
+        new_state_dict[name] = v
+    # load params
+    model.netG.load_state_dict(new_state_dict)
+
+    dummy = torch.randn(10, 3, 256, 256)
+    torch.onnx.export(model.netG, dummy, './out.onnx')
